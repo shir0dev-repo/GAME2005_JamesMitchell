44f25d10501e0a220189f3254ab215780ae280ad
James Mitchell <v010000010101001101010011@gmail.com>
2024-09-12 13:57:20 -0400

    solved quicksand issue
    thanks sarah :D

--- Assets/!Scripts/Bounce.cs
+++ Assets/!Scripts/Bounce.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    private float _velocity = 0.0f;
+    public float _velocity = 0.0f;
 
     private readonly float _sphereRadius = 0.5f;
+    private static readonly float _acceleration = Physics.gravity.y;
 
     private void FixedUpdate()
     {
-        _velocity += Physics.gravity.y * Time.fixedDeltaTime;
+        _velocity += _acceleration * Time.fixedDeltaTime;
 
         transform.position += Time.fixedDeltaTime * _velocity * Vector3.up;
 
-        if (transform.position.y < _sphereRadius && _velocity < 0f)
-            _velocity *= -1f;
+        if (transform.position.y < _sphereRadius)
+        {
+            transform.position.Set(transform.position.x, _sphereRadius + Mathf.Epsilon, transform.position.z);
+            _velocity = -_velocity - _acceleration * Time.fixedDeltaTime;
+        }
     }
 }
