486fea0aed02b02cebdeb86ba9ed58d9392dc169
James Mitchell <v010000010101001101010011@gmail.com>
2024-09-13 18:41:48 -0400

    final push for lab 2
    organized files

--- Assets/!Scripts/Editor/ReadOnlyDrawer.cs
+++ Assets/!Scripts/Editor/ReadOnlyDrawer.cs
@@ -3,25 +3,4 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        base.OnGUI(position, property, label);
-        GUI.enabled = false;
-
-        property.serializedObject.forceChildVisibility = true;
-        EditorGUI.PropertyField(position, property, label, true);
-        property.serializedObject.forceChildVisibility = false;
-        
-        GUI.enabled = true;
-
-
-    }
-}
