44212e398b37637f6a9e9c4c75e3de2a2c3c93bd
James Mitchell <v010000010101001101010011@gmail.com>
2024-09-13 21:18:34 -0400

    refactor finished

--- Assets/!Scripts/Movement/Bounce.cs
+++ Assets/!Scripts/Movement/Bounce.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bounce : MonoBehaviour
+public class Bounce : Movement
 {
+    [SerializeField] private float _planeHeightWS = 0f;
     [SerializeField, ReadOnly] private float _velocity = 0.0f;
 
     private readonly float _sphereRadius = 0.5f;
     private static readonly float _acceleration = Physics.gravity.y;
 
-    private void FixedUpdate()
+
+    protected override void Move()
     {
         _velocity += _acceleration * Time.fixedDeltaTime;
 
         transform.position += Time.fixedDeltaTime * _velocity * Vector3.up;
 
-        if (transform.position.y < _sphereRadius)
+        if (transform.position.y < _planeHeightWS + _sphereRadius)
         {
             transform.position.Set(transform.position.x, _sphereRadius + Mathf.Epsilon, transform.position.z);
             _velocity = -_velocity - _acceleration * Time.fixedDeltaTime;
