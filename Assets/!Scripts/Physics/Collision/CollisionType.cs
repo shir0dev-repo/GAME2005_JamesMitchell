af3a674463a15087ea285b8859640f3d8cbdfa0e
James Mitchell <v010000010101001101010011@gmail.com>
2024-10-11 19:55:24 -0400

    Lab 5 started

---
+++ Assets/!Scripts/Physics/Collision/CollisionType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Flags]
+public enum CollisionType 
+{
+    NONE = 0,
+    Sphere = 1, 
+    Cube = 2,
+    Plane = 4,
+    Halfspace = 8
+}
